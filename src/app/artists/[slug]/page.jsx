564402ff63decd91edfc04366bd6b793491c110d
Dannie Vinther <davi@kea.dk>
2024-10-28 14:21:55 +0100

    update next, lcss, isr, dynamic=false

--- src/app/artists/[slug]/page.jsx
+++ src/app/artists/[slug]/page.jsx
@@ -1,9 +1,18 @@
 import styles from "./artist.module.css";
 
+import { getData } from "@/lib/supabase.js";
 import { getDataById } from "@/lib/supabase.js";
 import Image from "next/image";
 
 export const revalidate = 3600;
+export const dynamicParams = false; // or false, to 404 on unknown paths
+
+export async function generateStaticParams() {
+  const artists = await getData("artists");
+  return artists.map((artist) => ({
+    slug: artist.slug,
+  }));
+}
 
 async function Artist({ params }) {
   const { slug } = await params;
